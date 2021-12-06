7daecdb35d11e380a4ef438bccc29cb0075d9bec
wenjunxp@sina.com <wenjunxp@sina.com>
2021-12-06 12:25:22 +0800

    first commit

---
+++ hellow/src/main/java/org/hellow/App.java
@@ -0,0 +1,13 @@
+package org.hellow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
