354b9016fd25e3bb9cc6c3e7198a695497445658
wenjunxp@sina.com <wenjunxp@sina.com>
2021-12-07 16:47:20 +0800

    拒绝消息到死信队列

---
+++ rabbitmq/src/main/java/org/rabbitmq/dead/ConsumerLen.java
@@ -0,0 +1,50 @@
+package org.rabbitmq.dead;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import org.rabbitmq.util.RabbitUtilMQUtils;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+
+public class ConsumerLen {
+	public static String NOMAL_EXCHANGE = "nomal-exchange";
+	public static String DEAD_EXCHANGE = "dead-exchange";
+
+	public static String NOMAL_QUEUE = "nomal-queue";
+	public static String DEAD_QUEUE = "dead-queue";
+
+	public static String NOMAL_QUEUE_ROUTE = "nomal-999";
+	public static String DEAD_QUEUE_ROUTE = "dead-999";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+
+		Channel channel = RabbitUtilMQUtils.getChannel();
+
+		channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
+		channel.exchangeDeclare(NOMAL_EXCHANGE, BuiltinExchangeType.DIRECT);
+
+		channel.queueDeclare(DEAD_QUEUE, false, false, false, null);
+		channel.queueBind(DEAD_QUEUE, DEAD_EXCHANGE, DEAD_QUEUE_ROUTE);
+
+		Map<String, Object> arguments = new HashMap<>();
+
+		arguments.put("x-dead-letter-exchange", DEAD_EXCHANGE);
+		arguments.put("x-dead-letter-routing-key", DEAD_QUEUE_ROUTE);
+		arguments.put("x-max-length", 6);
+		
+		channel.queueDeclare(NOMAL_QUEUE, false, false, false, arguments);
+		channel.queueBind(NOMAL_QUEUE, NOMAL_EXCHANGE, NOMAL_QUEUE_ROUTE);
+
+		System.out.println("Consumer01 等待接收消息............");
+		channel.basicConsume(NOMAL_QUEUE, true, (consumerTag, message) -> {
+			System.out.println("Consumer01接收的消息是:" + new String(message.getBody(), "UTF-8"));
+		}, consumerTag -> {
+
+		});
+
+	}
+}
---
+++ rabbitmq/src/main/java/org/rabbitmq/dead/Producer.java
@@ -0,0 +1,26 @@
+package org.rabbitmq.dead;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import org.rabbitmq.util.RabbitUtilMQUtils;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+
+public class Producer {
+	public static String NOMAL_EXCHANGE = "nomal-exchange";
+	public static String NOMAL_QUEUE_ROUTE = "nomal-999";
+	
+	public static void main(String[] args) throws IOException, TimeoutException {
+		Channel channel = RabbitUtilMQUtils.getChannel();
+		
+		for(int i=1;i<11;i++) {
+			String message="info-"+i;
+			channel.basicPublish(NOMAL_EXCHANGE, NOMAL_QUEUE_ROUTE, null, message.getBytes("UTF-8"));
+			
+		}
+	}
+}
